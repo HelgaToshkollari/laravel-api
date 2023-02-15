0971165280f2d1ee66a184cd6eee36635a46dc32
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-02-15 23:03:49 +0100

    adds index and show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index (){
+        $projects= Project::all();
+        return response()->json($projects);
+    }
+    public function show(Project $project){
+
+        $project->load("technologies","types");
+
+        return response()->json($project);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/projects", [ProjecController::class, "index"]);
-Route::get("/projects{project}", [ProjecController::class, "show"]);
+Route::get("/projects", [ProjectController::class, "index"]);
+Route::get("/projects/{project}", [ProjectController::class, "show"]);
