e1033b9a7f0a0bc1111dc79ab30ac7b47c673335
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-02-16 10:38:06 +0100

    fixed error on api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ProjectController;
+
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/projects", [ProjectController::class, "index"]);
+Route::get("/projects", [ApiProjectController::class, "index"]);
 Route::get("/projects/{project}", [ProjectController::class, "show"]);
