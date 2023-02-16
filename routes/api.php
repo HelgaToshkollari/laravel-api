fe08202a42da08722939b003dbda82057e971fdf
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-02-16 10:52:39 +0100

    small change

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/projects", [ApiProjectController::class, "index"]);
+Route::get("/projects", [ProjectController::class, "index"]);
 Route::get("/projects/{project}", [ProjectController::class, "show"]);
